546c1085720b80b0525d02bfd825bc32abbd2b34
Eduardo De La Cruz <eduardo.delacruz@gft.com>
2018-11-26 12:18:29 +0100

    Initial Commit

---
+++ ShimmerAnimation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ShimmerAnimation
+//
+//  Created by De La Cruz, Eduardo on 26/11/2018.
+//  Copyright © 2018 De La Cruz, Eduardo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
